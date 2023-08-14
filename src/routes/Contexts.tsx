2f330165f34fe83817aa386eafefd3ae08eb8178
Enchanter <developer@enchanter.com>
2023-08-14 13:31:50 +0800

    feat: use local pics

--- src/routes/Contexts.tsx
+++ src/routes/Contexts.tsx
@@ -10,8 +10,11 @@ interface UserState {
   logout: () => void;
 }
 
-export const fAvatar = 'https://xsgames.co/randomusers/assets/avatars/pixel/33.jpg';
-export const mAvatar = 'https://xsgames.co/randomusers/assets/avatars/pixel/34.jpg';
+// export const fAvatar = 'https://xsgames.co/randomusers/assets/avatars/pixel/33.jpg';
+// export const mAvatar = 'https://xsgames.co/randomusers/assets/avatars/pixel/34.jpg';
+
+export const fAvatar = '33.jpg';
+export const mAvatar = '34.jpg';
 
 const UserContext = React.createContext<UserState|null>(null);
 
